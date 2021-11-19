4eef3b3022948216697e369e1f56f4a4aee47ed6
Ryan Ashby <rjapsu160@gmail.com>
2021-11-19 14:45:11 -0800

    add functionality for choices based upon if/else if statements.

--- src/index.js
+++ src/index.js
@@ -13,16 +13,53 @@ $(document).ready(function() {
     let promise = CurrencyService.getExchange();
     console.log(choice);
     console.log(amount);
-    promise.then(function(response) {
-      const responseGiven = JSON.parse(response);
-      // let error = responseGiven.error-type;
-      $('#result').html(`${responseGiven}`);
-      $('#countryChoice').text(`${choice}`);
-    }, function(error) {
-      $('#anyError').text(`There was an error processing your request ${error}`);
-    });
+    if (choice === "AUD") {
+      promise.then(function(response) {
+        const responseGiven = JSON.parse(response);
+        $('#result').text((amount * `${responseGiven.conversion_rates.AUD}`).toFixed(2));
+        $('#countryChoice').text(`${choice}`);
+      }, function(error) {
+        $('#anyError').text(`There was an error processing your request ${error}`);
+      });
+    } else if (choice === "CAD") {
+      promise.then(function(response) {
+        const responseGiven = JSON.parse(response);
+        $('#result').text((amount * `${responseGiven.conversion_rates.CAD}`).toFixed(2));
+        $('#countryChoice').text(`${choice}`);
+      }, function(error) {
+        $('#anyError').text(`There was an error processing your request ${error}`);
+      });
+    } else if (choice === "GBP") {
+      promise.then(function(response) {
+        const responseGiven = JSON.parse(response);
+        $('#result').text((amount * `${responseGiven.conversion_rates.GBP}`).toFixed(2));
+        $('#countryChoice').text(`${choice}`);
+      }, function(error) {
+        $('#anyError').text(`There was an error processing your request ${error}`);
+      });
+    } else if (choice === "EUR") {
+      promise.then(function(response) {
+        const responseGiven = JSON.parse(response);
+        $('#result').text((amount * `${responseGiven.conversion_rates.EUR}`).toFixed(2));
+        $('#countryChoice').text(`${choice}`);
+      }, function(error) {
+        $('#anyError').text(`There was an error processing your request ${error}`);
+      });
+    } else if (choice === "CHF") {
+      promise.then(function(response) {
+        const responseGiven = JSON.parse(response);
+        $('#result').text((amount * `${responseGiven.conversion_rates.CHF}`).toFixed(2));
+        $('#countryChoice').text(`${choice}`);
+      }, function(error) {
+        $('#anyError').text(`There was an error processing your request ${error}`);
+      });
+    } else {
+      promise.then(function(response) {
+        const responseGiven = JSON.parse(response);
+        $('#result').text(`${responseGiven.error}`);
+      }, function(error) {
+        $('#anyError').text(`There was an error processing your request ${error}`);
+      });
+    }
   });
 });
-
-
-
