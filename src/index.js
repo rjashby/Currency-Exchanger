961dc0984ce2b68435b6e222c810202baff0794b
Ryan Ashby <rjapsu160@gmail.com>
2021-11-19 09:57:10 -0800

    add click function code.

--- src/index.js
+++ src/index.js
@@ -4,3 +4,17 @@ import 'bootstrap/dist/css/bootstrap.min.css';
 import './css/styles.css';
 import CurrencyService from './currency-service';
 
+$(document).ready(function() {
+  $('#calc').click(function() {
+    let amount = $('#amount').val();
+    let choice = $('#country').val();
+    let promise = CurrencyService.getExchange(amount);
+    promise.then(function(response) {
+      const body = JSON.parse(response);
+      $('#result').html(`${body.main.humidity}%`);
+      $('.showTemp').text(`The temperature in Kelvins is ${body.main.temp} degrees.`);
+    }, function(error) {
+      $('.showErrors').text(`There was an error processing your request: ${error}`);
+    });
+  });
+});
